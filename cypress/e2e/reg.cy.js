9647d2e869f615467b2824944b5e3998cfa2e726
Siddharth190421 <110586836+Siddharth190421@users.noreply.github.com>
2025-01-28 17:17:28 +0530

    Delete cypress/e2e/reg.cy.js

--- cypress/e2e/reg.cy.js
+++
@@ -1,7 +0,0 @@
-describe('rege', () => {
-    it("regestration",()=>{
-        cy.visit("https://mailosaur.com/app/signup");
-        cy.get("#email").type("arrive-involved@ddqjhhki.mailosaur.net");
-        cy.get(`[type="submit"]`).contains("Create your account").click();
-    })
-})
